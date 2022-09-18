47e6358b2a9963f59b9ad5c0cc9667205bee1fd9
Рабочая <yuo@exemple.com>
2022-09-19 01:34:48 +0700

    проверка чет-нечет

---
+++ task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №6. Является ли принятое число четным
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+double rem = number % 2;
+
+if (rem == 0)
+{
+   Console.WriteLine("Это ЧЕТНОЕ число"); 
+}
+else
+{
+   Console.WriteLine("Это НЕЧЕТНОЕ число");
+}
