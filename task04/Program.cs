dbb3f8eedb646220c4f1cf760dee185db400f8da
Рабочая <yuo@exemple.com>
2022-09-19 00:44:24 +0700

    максимум из трех

---
+++ task04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №4. Найти максимум из трех чисел
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int maxnumber = number1;
+
+if (number2 > maxnumber) maxnumber = number2;
+if (number3 > maxnumber) maxnumber = number3;
+
+    Console.Write("Max = ");
+    Console.WriteLine(maxnumber);
+   
