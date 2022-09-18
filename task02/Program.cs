5774d13f627a523eca55308ce32e83c58da39fe7
Рабочая <yuo@exemple.com>
2022-09-19 00:56:42 +0700

    ввела переменные макс и мин

--- task02/Program.cs
+++ task02/Program.cs
@@ -5,17 +5,20 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+int maxnumber, minnumber;
+
 if (number1 > number2)
 {
-    Console.Write("Max = ");
-    Console.WriteLine(number1);
-    Console.Write("Min = ");
-    Console.WriteLine(number2);
+    maxnumber = number1;
+    minnumber = number2;
 }
 else
 {
-    Console.Write("Max = ");
-    Console.WriteLine(number1);
-    Console.Write("Min = ");
-    Console.WriteLine(number2);
+    maxnumber = number2;
+    minnumber = number1; 
 }
+
+Console.Write("Max = ");
+Console.WriteLine(maxnumber);
+Console.Write("Min = ");
+Console.WriteLine(minnumber);
