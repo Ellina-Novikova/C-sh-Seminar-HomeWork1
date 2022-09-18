ad7e1482da9db1156440fb1aa2176a7169cd338b
Рабочая <yuo@exemple.com>
2022-09-19 00:09:10 +0700

    вывод макс и мин без доп переменных

---
+++ task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №2. Определить максимальное и минимальное из двух чисел
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+    Console.Write("Min = ");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+    Console.Write("Min = ");
+    Console.WriteLine(number2);
+}
