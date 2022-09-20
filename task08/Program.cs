6117b4b823db7fe2e4d0de46909d1c0366095764
Рабочая <yuo@exemple.com>
2022-09-20 12:46:25 +0700

    Четные от 1 до N

---
+++ task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №8. Показать все четные числа от 1 до N
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int n = 2;
+if (n <= N)
+{
+    Console.WriteLine("Четные числа от 1 до " + N + ":");
+    while (n <= N)
+{
+   
+   Console.WriteLine(n); 
+   n += 2;
+}
+}
+else Console.WriteLine("Нет четных чисел от 1 до " + N);
