f7d1ed208595021cacaddeb285f899cf150d39de
Рабочая <yuo@exemple.com>
2022-09-22 20:04:51 +0700

    task10 методы EnterNumber и SecondDigit

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №10. Принимает на вход трехзначное число, 
+// на выходе показывает вторую цифру этого числа.
+
+int EnterNumber()
+{
+    Console.WriteLine("Введите число от 100 до 999");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SecondDigit(int number)
+{
+    int Decimal = (number % 100) / 10;
+    return Decimal;     
+}
+
+int a = EnterNumber();
+Console.WriteLine(SecondDigit(a));
