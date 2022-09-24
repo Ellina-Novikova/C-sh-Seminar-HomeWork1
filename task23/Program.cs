ebc9727fdeb1065a38078c66f8404a5873bf6922
Рабочая <yuo@exemple.com>
2022-09-24 23:01:34 +0700

    Таблица кубов от 1 до N

---
+++ task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №23. Принимает число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void TabCube(int number)
+{
+    Console.WriteLine("Таблица кубов от 1 до " + number + ":");
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine(count + " в кубе = " + Math.Pow(count, 3));
+        count ++;
+    }
+}
+
+TabCube(GetNumber());
