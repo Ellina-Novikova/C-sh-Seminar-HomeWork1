44eb595b6fdf0e86cefa84e0e0bb0d3face6ec19
Рабочая <yuo@exemple.com>
2022-09-28 23:32:06 +0700

    сумма цифр числа

---
+++ task27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetDigCount(int num)  // количество цифр в числе
+{
+    int count = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num = num / 10;
+        count ++;
+    }
+    return count;
+}
+
+int SummDigit(int number)   // Сумма цифр числа
+{
+    int summ = 0;
+    int digree = GetDigCount(number);
+    Console.WriteLine(digree);
+    for (int i = 1; i <= digree; i++)
+    {
+        summ = summ + (number % 10);
+        number = number / 10;
+    }
+    return summ;
+}
+
+int A = GetNumber();
+Console.WriteLine($"Сумма цифр числа {A} = {SummDigit(A)}");
+
+
+
